00a0fa0c1810ce7367782ffeae2a2fb967a7a40e
colinplancich <colin.plancich@gmail.com>
2022-10-10 17:40:33 -0700

    Re-arranged some lines in Thought.js model

--- models/Thought.js
+++ models/Thought.js
@@ -1,21 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-const thoughtSchema = new Schema(
-  {
-    thoughtText: { type:String, required:true, minLength:1, maxLength:280 },
-    createdAt: { type:Date, default: Date.now, 
-    // TODO: getter method to format timestamp on query
-    },
-    username: { type:String, required:true},
-    reactions: [reactionSchema],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    }
-  }
-);
-
 const reactionSchema = new Schema(
     {
         reactionId: { type:Schema.Types.ObjectId, default: () => new Types.ObjectId() },
@@ -38,6 +22,22 @@ const reactionSchema = new Schema(
           id: false,
         }
 )
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type:String, required:true, minLength:1, maxLength:280 },
+    createdAt: { type:Date, default: Date.now, 
+    // TODO: getter method to format timestamp on query
+    },
+    username: { type:String, required:true},
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    }
+  }
+);
 
 thoughtSchema
   .virtual('reactionCount')
